c30c75bf5b5e368380a599e0f2a77ffb6a070bfb
HugoCruzEng <hugocruz.eng@gmail.com>
2023-09-06 16:47:54 -0300

    CFM REU 06SET2023

--- start.m
+++ start.m
@@ -2,5 +2,16 @@
 H=1;
 Fg=1;
 Js=1;
- 
-open Rudder_system_control2020.slx
+
+%Table 3: List of ship design parameters 
+Izz=5500; %inercia
+c=1;
+ro=1000;
+d=1.6; %diametro do rudder
+ek1=0.0375;
+vship=1;
+mrudder=1000; %massa Rudder em kg
+
+%Table 4: List of actuator and gyroscope parameters
+Res1=1;
+Res2=1;
