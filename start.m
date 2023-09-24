d652b3e5573ddc118e90648188495291ffab0d7c
victo <victo@192.168.0.28>
2023-09-24 20:46:01 -0300

    trabalho do dia 24SET

--- start.m
+++ start.m
@@ -13,8 +13,13 @@ vship=1;
 mrudder=1000; %massa Rudder em kg
 
 %Table 4: List of actuator and gyroscope parameters
-Res1=1;
-Res2=1;
+Res1=1; %ohm
+Res2=1; %ohm
+Res3=1; %ohm
+Res4=10; %ohm
+Res5=3.4; %ohm
+Res6=1; %ohm
+Res7=1; %ohm
 
 HydraulicPressure=10^5; %Pa
 ValvePassageMaximumArea=4.05*10^(-5); %m2
