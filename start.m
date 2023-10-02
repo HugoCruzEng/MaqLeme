7808b2c6ebcc4bd3c46dd48ee91d2d233c2fd12e
victo <victo@192.168.0.28>
2023-10-01 21:34:23 -0300

    elaboracao da versao _3

--- start.m
+++ start.m
@@ -4,13 +4,13 @@ Fg=1;
 Js=1;
 
 %Table 3: List of ship design parameters 
-Izz=5500; %inercia do leme
+Izz=0.2; %inercia do leme
 c=1;
 ro=1000;
-d=1.6; %diametro do rudder
+d=0.7; %diametro do rudder
 ek1=0.0375;
 vship=1;
-mrudder=1000; %massa Rudder em kg
+mrudder=50; %massa Rudder em kg
 
 %Table 4: List of actuator and gyroscope parameters
 Res1=1; %ohm
